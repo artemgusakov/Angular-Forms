9fb11c61100bd9cf5fa746f9fdb954781ac393c6
artem <artyom.gusakov@gmail.com>
2019-05-05 21:55:21 +0300

    _.clone(this.originalUserSetting)

---
+++ src/app/data/user-settings.ts
@@ -0,0 +1,6 @@
+export interface UserSettings {
+  name: string;
+  emailOffers: boolean;
+  interfaceStyle: string;
+  subscriptionType: string;
+}
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserSettings} from '../data/user-settings';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsFormComponent implements OnInit {
 
+  private _originalUserSetting: UserSettings = {
+    name: 'Test',
+    emailOffers: true,
+    interfaceStyle: 'medium',
+    subscriptionType: 'Monthly'
+  };
+
+  public userSettings: UserSettings = _.clone(this._originalUserSetting);
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  cancelClick() {
+    this.userSettings = _.clone(this._originalUserSetting);
+  }
 }
